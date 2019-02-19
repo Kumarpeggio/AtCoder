ec96b7243ec5c70a5fb0caf38c0282fa91257320
Yuta Kumakura <kumarpeggio.of.bluesteel@gmail.com>
2019-02-19 14:11:53 +0900

    create Folder

---
+++ AtCoder Beginners Selection/ABC086A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  
+  int a, b, c;
+  
+  cin >> a >> b;
+  
+  c = a * b;
+  
+  if (c % 2 == 1) {
+    
+    cout << "Odd" << endl;
+    
+  } else {
+    
+    cout << "Even" << endl;
+    
+  }
+  
+}
