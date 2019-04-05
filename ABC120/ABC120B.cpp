28e6b6d11ba82c88987821876253d7151b61d7f2
Yuta Kumakura <kumarpeggio.of.bluesteel@gmail.com>
2019-04-05 15:39:27 +0900

    K-th Common Divisor

---
+++ ABC120/ABC120B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  
+  int A, B, K;
+  cin >> A >> B >> K;
+  
+  for (int i = min(A, B); i > 0; i--) {
+    if (A % i == 0 && B % i == 0) {
+      K--;
+      if (K == 0) {
+        cout << i << endl;
+        break;
+      }
+    }
+  }
+  
+}
